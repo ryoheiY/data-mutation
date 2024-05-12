80bcc4fafe276a6de0f07fb8f89a43da18b9968d
ryoheiY <momoyamashia@icloud.com>
2024-05-13 00:50:02 +0900

    useActionState実装前のコミット

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,13 +1,33 @@
 import { storePost } from '@/lib/posts';
 import {redirect} from "next/navigation";
 import {FormSubmit} from "@/components/form-submit";
+import {useActionState} from "react";
 
 export default function NewPostPage() {
+
+  const actionState = useActionState();
+
   async function createPost(formData) {
     "use server";
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
+
+    let errors = [];
+
+    if( !title || title.trim().length === 0 ) {
+      errors.push("Please enter a title");
+    }
+    if( !content || content.trim().length === 0 ) {
+      errors.push("Please enter a content");
+    }
+    if(!image) {
+      errors.push("Please select a image");
+    }
+
+    if(errors.length > 0) {
+      return { errors };
+    }
 
     await storePost({
       imageUrl: '',
@@ -25,7 +45,7 @@ export default function NewPostPage() {
       <form action={createPost}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
+          <input type="text" id="title" name="title" required />
         </p>
         <p className="form-control">
           <label htmlFor="image">Image URL</label>
