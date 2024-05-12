09a2ee48570feaf3f60103b3b7ab9a2aa0583372
ryoheiY <momoyamashia@icloud.com>
2024-05-13 01:24:10 +0900

    useFormStateを実装

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,31 +1,30 @@
+import { redirect } from 'next/navigation';
+
 import { storePost } from '@/lib/posts';
-import {redirect} from "next/navigation";
-import {FormSubmit} from "@/components/form-submit";
-import {useActionState} from "react";
+import PostForm from '@/components/post-form';
 
 export default function NewPostPage() {
-
-  const actionState = useActionState();
-
-  async function createPost(formData) {
-    "use server";
+  async function createPost(prevState, formData) {
+    'use server';
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
 
     let errors = [];
 
-    if( !title || title.trim().length === 0 ) {
-      errors.push("Please enter a title");
-    }
-    if( !content || content.trim().length === 0 ) {
-      errors.push("Please enter a content");
-    }
-    if(!image) {
-      errors.push("Please select a image");
+    if (!title || title.trim().length === 0) {
+      errors.push('Title is required.');
     }
 
-    if(errors.length > 0) {
+    if (!content || content.trim().length === 0) {
+      errors.push('Content is required.');
+    }
+
+    if (!image || image.size === 0) {
+      errors.push('Image is required.');
+    }
+
+    if (errors.length > 0) {
       return { errors };
     }
 
@@ -33,37 +32,11 @@ export default function NewPostPage() {
       imageUrl: '',
       title,
       content,
-      userId: 1
-    })
+      userId: 1,
+    });
 
     redirect('/feed');
   }
 
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" required />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+  return <PostForm action={createPost} />;
 }
